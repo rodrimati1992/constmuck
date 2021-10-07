3a2a31f2c4d878fd067d24572c3ee88f9385a941
rodrimati1992 <rodrimatt1985@gmail.com>
2021-10-07 04:13:40 -0300

    Changed ImplsCopy bound to `Pod`
    Updated tests, docs, and examples that use ImplsCopy

--- src/copying.rs
+++ src/copying.rs
@@ -11,23 +11,23 @@ use crate::TypeSize;
 pub(crate) mod impls_copy {
     use super::*;
 
-    /// Encodes a `T: Copy` bound as a value,
-    /// avoids requiring (unstable as of 2021) trait bounds in `const fn`s.
+    /// Encodes that a `T` is trivially copyable,
+    /// avoiding requiring (unstable as of 2021) trait bounds in `const fn`s.
     ///
     /// Related: the [`copying`](crate::copying) module
     ///
-    /// # Deprecation
+    /// # Bound
     ///
-    /// Copying `&T` with the approach constmuck uses
-    /// (an intermediate `MaybeUninit<[u8; N]>`)
-    /// isn't settled as being sound,
-    /// so [`ImplsCopy`] will require [`Pod`] in the 0.2.0 version.
+    /// This currently requires [`Pod`]
+    /// because the approach constmuck has for copying uses
+    /// an intermediate `MaybeUninit<[u8; N]>`,
+    /// which isn't settled as being sound to use with pointer types,
+    /// but is guaranteed sound for [`Pod`] types.
     ///
     /// If there's a more permissive bound that allows more non-pointer-containing
     /// `Copy` types, `ImplsCopy` will be changed to use that.
     ///
     /// [`Pod`]: bytemuck::Pod
-    #[deprecated(since = "0.1.1", note = "too permissive in what it allows copying")]
     pub struct ImplsCopy<T> {
         _private: PhantomData<fn() -> T>,
     }
@@ -40,7 +40,7 @@ pub(crate) mod impls_copy {
         }
     }
 
-    impl<T: Copy> ImplsCopy<T> {
+    impl<T: crate::Pod> ImplsCopy<T> {
         /// Constructs an `ImplsCopy`
         ///
         /// You can also use the [`infer`] macro to construct `ImplsCopy` arguments.
@@ -54,12 +54,15 @@ pub(crate) mod impls_copy {
             _private: PhantomData,
         };
 
-        /// Constructs an `ImplsCopy<T>` without checking that `T` implements [`Copy`].
+        /// Constructs an `ImplsCopy<T>` without checking that `T`
+        /// implements [`Copy`] + [`Pod`].
         ///
         /// # Safety
         ///
         /// You must ensure that `T` is safe to `memcpy` without forgetting the
-        /// copied-from value.
+        /// copied-from value, and doesn't contain pointers of any kind.
+        ///
+        /// [`Pod`]: bytemuck::Pod
         #[inline(always)]
         pub const unsafe fn new_unchecked() -> Self {
             Self::__NEW_UNCHECKED__
@@ -70,13 +73,14 @@ pub(crate) mod impls_copy {
 #[doc(no_inline)]
 pub use crate::ImplsCopy;
 
-impl<T: Copy> crate::Infer for ImplsCopy<T> {
+impl<T: crate::Pod> crate::Infer for ImplsCopy<T> {
     const INFER: Self = Self::NEW;
 }
 
 /// Copies a `T` from a `&T`
 ///
-/// Requires that `T` implements `Copy`
+/// Requires that `T` implements `Copy + Pod`
+/// (see [`ImplsCopy`] docs for why it requires `Pod`)
 ///
 /// # Example
 ///
@@ -94,11 +98,8 @@ impl<T: Copy> crate::Infer for ImplsCopy<T> {
 /// }
 ///
 /// const PAIR_U8: [u8; 2] = pair(&128, type_size!(u8));
-/// const PAIR_STR: [&str; 2] = pair(&"hello", type_size!(&str));
 ///
 /// assert_eq!(PAIR_U8, [128, 128]);
-///
-/// assert_eq!(PAIR_STR, ["hello", "hello"]);
 ///
 /// ```
 pub const fn copy<T, const SIZE: usize>(reff: &T, bounds: TypeSize<ImplsCopy<T>, T, SIZE>) -> T {
@@ -113,7 +114,8 @@ pub const fn copy<T, const SIZE: usize>(reff: &T, bounds: TypeSize<ImplsCopy<T>,
 
 /// Creates a `[T; ARR_LEN]` by copying from a `&T`
 ///
-/// Requires that `T` implements `Copy`
+/// Requires that `T` implements `Copy + Pod`
+/// (see [`ImplsCopy`] docs for why it requires `Pod`)
 ///
 /// To specify the length of the returned array,
 /// [`TypeSize::repeat`] can be used instead.
@@ -123,17 +125,12 @@ pub const fn copy<T, const SIZE: usize>(reff: &T, bounds: TypeSize<ImplsCopy<T>,
 /// ```rust
 /// use constmuck::{copying, type_size};
 ///
-/// const PAIR: [Option<u8>; 2] = copying::repeat(&None, type_size!(Option<u8>));
-/// const TEN: [&str; 10] = copying::repeat(&"world", type_size!(&str));
+/// const PAIR: [[u8; 2]; 2] = copying::repeat(&[3, 5], type_size!([u8; 2]));
 ///
-/// assert_eq!(PAIR, [None, None]);
+/// assert_eq!(PAIR, [[3, 5], [3, 5]]);
 ///
 /// // you can use `TypeSize::repeat` like here to pass the length of the returned array.
-/// assert_eq!(type_size!(Option<u8>).repeat::<2>(&None), [None, None]);
-///
-///
-/// assert_eq!(TEN, ["world"; 10]);
-/// assert_eq!(type_size!(&str).repeat::<10>(&"world"), ["world"; 10]);
+/// assert_eq!(type_size!([u8; 2]).repeat::<2>(&[3, 5]), [[3, 5], [3, 5]]);
 ///
 /// ```
 pub const fn repeat<T, const SIZE: usize, const ARR_LEN: usize>(
